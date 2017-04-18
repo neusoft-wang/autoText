ae643e96e2a34e7a4867a5d5dfea78c244d3c2eb
e <live609450148@qq.com>
2017-04-18 13:38:03 +0800

    first commit

---
+++ untitled/src/main/java/com/selenium/test/utils/Yangjun.java
@@ -0,0 +1,79 @@
+package com.selenium.test.utils;
+
+/**
+ * Created by live6 on 2017/4/5.
+ */
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Yangjun {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.baidu.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testYangjun() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.id("kw")).click();
+        driver.findElement(By.id("kw")).clear();
+        driver.findElement(By.id("kw")).sendKeys("杨俊");
+        driver.findElement(By.id("su")).click();
+        driver.findElement(By.linkText("杨俊_百度百科")).click();
+        driver.findElement(By.linkText("基本信息")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
